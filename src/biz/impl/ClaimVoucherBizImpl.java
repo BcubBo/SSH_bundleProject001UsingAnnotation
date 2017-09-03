41e8f89382aeaf92c43004f2d50adab642cf39c6
bowieu <fatalitywolf@foxmail.com>
2017-09-03 21:34:54 +0800

    添加事务，并设置不添加事务的方法，更改注入的方式为type类型注入防止名称不匹配

--- src/biz/impl/ClaimVoucherBizImpl.java
+++ src/biz/impl/ClaimVoucherBizImpl.java
@@ -2,11 +2,13 @@ package biz.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import biz.ClaimVoucherBiz;
 import dao.ClaimVoucherDao;
 import po.BizClaimVoucher;
 @Service
+@Transactional
 public class ClaimVoucherBizImpl implements ClaimVoucherBiz {
 	private ClaimVoucherDao claimVoucherDao;
 	@Override
--- src/biz/impl/EmployeeBizImpl.java
+++ src/biz/impl/EmployeeBizImpl.java
@@ -2,17 +2,21 @@ package biz.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import biz.EmployeeBiz;
 import dao.EmployeeDao;
 import po.SysEmployee;
 
 @Service
+@Transactional//所有的公共类都会添加事务属性为默认值
 public class EmployeeBizImpl implements EmployeeBiz {
 	
 	private EmployeeDao employeeDao;
 
 	@Override
+	@Transactional(propagation=Propagation.SUPPORTS)//默认不使用事务，但是如果有也不拒绝。把最顶层的配置给覆盖掉了
 	public SysEmployee checkLogin(SysEmployee emp) {
 		SysEmployee result = employeeDao.findById(emp.getSn());
 		if(result!=null && result.getPassword().equals(emp.getPassword())) {
